e5009ece2b41453a71b2709bb90c97ea28d38727
Satya Chaitanya <66238780+bymeSatya@users.noreply.github.com>
2024-06-01 14:47:45 +0530

    Update mvs_app.py

--- mvs_app.py
+++ mvs_app.py
@@ -12,14 +12,16 @@ selected_movie = st.selectbox('Choose Movie for Recommendation',movies_list['tit
 from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer(max_features=5000,stop_words='english')
 
-vectors = cv.fit_transform(new_df['tags']).toarray()
+vectors = cv.fit_transform(movies_list['tags']).toarray()
 
 from sklearn.metrics.pairwise import cosine_similarity
 similarity = cosine_similarity(vectors)
 
 def recommender(movie):
-    movie_index = new_df[new_df['title'] == movie].index[0]
+    movie_index = movies_list[movies_list['title'] == movie].index[0]
     distances = similarity[movie_index]
     movie_list = sorted(list(enumerate(similarity[movie_index])),reverse = True,key = lambda x:x[1])[1:6]
     for i in movie_list:
         print(new_df.iloc[i[0]].title)
+
+recommender(selected_movie)
