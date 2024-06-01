2a6b22f6c74415d7f812f083d9e5bc8915e79921
Satya Chaitanya <66238780+bymeSatya@users.noreply.github.com>
2024-06-01 14:16:12 +0530

    Update mvs_app.py

--- mvs_app.py
+++ mvs_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import ast
-#import nltk
-#from nltk.stem.porter import PorterStemmer
+import nltk
+from nltk.stem.porter import PorterStemmer
 
 st.title('Movies Recommender')
 movies_list = pd.read_csv('movies_list.csv')
