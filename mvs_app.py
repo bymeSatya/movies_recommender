a3a63881084aa21784cfa5be874823a553b24aa3
Satya Chaitanya <66238780+bymeSatya@users.noreply.github.com>
2024-06-01 14:07:16 +0530

    Update mvs_app.py

--- mvs_app.py
+++ mvs_app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import ast
+import nltk
+from nltk.stem.porter import PorterStemmer
 
 st.title('Movies Recommender')
+movies_list = pd.read_csv('movies_list.csv')
 option = st.selectbox('Choose Movie for Recommendation','Movie1,Movie2')
